7f8735b9930fd127b7e3b28320dc45c90dd52b5a
Shivam Gupta <shivamgupta191092005@gmail.com>
2025-09-04 22:39:29 +0530

    Refactor ProductCard to use Next.js Image component for better performance; update next.config.ts to include remote image patterns

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { useCart } from "@/context/CartContext";
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -10,7 +11,14 @@ export default function ProductCard({ product }: { product: any }) {
     return (
         <Card className="p-4 flex flex-col justify-between">
             <Link href={`/product/${product.id}`}>
-                <img src={product.image} alt={product.title} className="h-40 mx-auto" />
+                <Image
+                    src={product.image}
+                    alt={product.title}
+                    width={160}        // required
+                    height={160}       // required
+                    className="h-40 w-auto mx-auto object-contain"
+                />
+
                 <CardContent>
                     <h3 className="font-bold line-clamp-1">{product.title}</h3>
                     <p className="text-sm text-gray-500">{product.category}</p>
